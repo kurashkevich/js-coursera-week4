11c438b773d1dbf08cbe1a330ef29835170c6b8f
vlad <v.kurashkevich@gmail.com>
2018-08-30 06:32:02 +0300

    actual code

--- js/week3/js/index.js
+++ js/week3/js/index.js
@@ -1,186 +1,265 @@
-/*
-* Реализовать 2 функции, add and subtract
-* функции могут принимать в качестве параметров 4 типа (минуты, дни, часы, месяца)
-*
-*
-*123123
-* */
-function createObject(date) {
-    return {
-        add: function (val, type) {
-            date = new Date(date);
-            var newDate;
-           switch (type) {
-                case 'days': {
-                    newDate = new Date(date.getFullYear(), date.getMonth(), date.getDate() + val, date.getHours(), date.getMinutes());
-                    console.log('add days' + newDate);
-                    break;
-                }
-                case 'months': {
-                    newDate = new Date(date.getFullYear(), date.getMonth() + val, date.getDate(), date.getHours(), date.getMinutes());
-                    console.log('add month');
-                    break;
-                }
-                case 'hours': {
-                    newDate = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours() + val, date.getMinutes());
-                    break;
-                }
-                case 'minutes': {
-                    newDate = new Date(date.getFullYear(), date.getMonth() + val, date.getDate(), date.getHours(), date.getMinutes() + val);
-                    console.log('add min');
-                    break;
+//Написать объект  +
+// Написать для него функции +
+// Функция проверки корректности данных +
+// функция для парсинга ( использовать регулярки)
 
-                }
-            }
-           time.value = newDate;
-           return time;
-
-        },
-        subtract: function (val, type) {
-            date = new Date(date);
-            var newDate;
-            switch (type) {
-                case 'days':{
-                    newDate = new Date(date.getFullYear(), date.getMonth(), date.getDate()-val, date.getHours(), date.getMinutes());
-                    break;
-                }
-                case 'months':{
-                    newDate = new Date(date.getFullYear(), date.getMonth()-val, date.getDate(), date.getHours(), date.getMinutes());
-                    break;
-                }
-                case 'hours':{
-                    newDate = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours()-val, date.getMinutes());
-                    break;
-                }
-                case 'minutes':{
-                    newDate = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes()-val);
-                    break;
-
-                }
-            }
-            time.value = newDate;
-            return time;
-        }
+var time = {
+    value: '',
+    getValue: function () {
+        return this.value;
+    },
+    setValue: function (value) {
+        this.value = value;
+        return this;
+    },
+    add: function (units, type) {
+        checkData(units, type);
+        addUnits(units, type);
+        return this;
+    },
+    subtract: function (units, type) {
+        checkData(units, type);
+        subtractUnits(units, type);
+        return this;
     }
 }
 
-
-
-//addTest(2, 'days', '2017-05-12 13:45');
-
- //   .add(24, 'hours')
-    //.subtract(1, 'months')
-    //.add(3, 'days')
-   // .add(15, 'minutes');
-
-time = {
-    _value: '2017-05-12 13:45',
-    date: function (str){
-        return {
-            add: function (val, type) {
-                str = new Date(str);
-                var newDate;
-                switch (type) {
-                    case 'days': {
-                        newDate = new Date(str.getFullYear(), str.getMonth(), str.getDate() + val, str.getHours(), str.getMinutes());
-                        console.log('add days' + newDate);
-                        break;
-                    }
-                    case 'months': {
-                        newDate = new Date(str.getFullYear(), str.getMonth() + val, str.getDate(), str.getHours(), str.getMinutes());
-                        console.log('add month');
-                        break;
-                    }
-                    case 'hours': {
-                        newDate = new Date(str.getFullYear(), str.getMonth(), str.getDate(), str.getHours() + val, str.getMinutes());
-                        break;
-                    }
-                    case 'minutes': {
-                        newDate = new Date(str.getFullYear(), str.getMonth() + val, str.getDate(), str.getHours(), str.getMinutes() + val);
-                        console.log('add min');
-                        break;
-
-                    }
-                }
-                time.value = newDate;
-                console.log(this.value + ' :add');
-                return this;
-            },
-            second: function () {
-                console.log('sec');
-                return this;
-            }
+function addUnits(units, type) {
+    var str = new Date(time.value);
+    var newDate;
+    switch (type) {
+        case 'days': {
+            newDate = new Date(str.getFullYear(), str.getMonth(), str.getDate() + units, str.getHours(), str.getMinutes());
+            break;
         }
-
+        case 'months': {
+            newDate = new Date(str.getFullYear(), str.getMonth() + units, str.getDate(), str.getHours(), str.getMinutes());
+            break;
+        }
+        case 'hours': {
+            newDate = new Date(str.getFullYear(), str.getMonth(), str.getDate(), str.getHours() + units, str.getMinutes());
+            break;
+        }
+        case 'minutes': {
+            newDate = new Date(str.getFullYear(), str.getMonth(), str.getDate(), str.getHours(), str.getMinutes() + units);
+            break;
+        }
     }
-    /*add: function (val, type) {
-        var date = new Date(date);
-        var newDate;
-        switch (type) {
-            case 'days': {
-                newDate = new Date(date.getFullYear(), date.getMonth(), date.getDate() + val, date.getHours(), date.getMinutes());
-                console.log('add days' + newDate);
-                break;
-            }
-            case 'months': {
-                newDate = new Date(date.getFullYear(), date.getMonth() + val, date.getDate(), date.getHours(), date.getMinutes());
-                console.log('add month');
-                break;
-            }
-            case 'hours': {
-                newDate = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours() + val, date.getMinutes());
-                break;
-            }
-            case 'minutes': {
-                newDate = new Date(date.getFullYear(), date.getMonth() + val, date.getDate(), date.getHours(), date.getMinutes() + val);
-                console.log('add min');
-                break;
+    time.value = newDate;
+    parseDate();
+}
 
-            }
+function subtractUnits(units, type){
+    var str = new Date(time.value);
+    var newDate;
+    switch (type) {
+        case 'days': {
+            newDate = new Date(str.getFullYear(), str.getMonth(), str.getDate() - units, str.getHours(), str.getMinutes());
+            break;
         }
-        time.value = newDate;
-        return this;
-
-    }*/    /*add: function (val, type) {
-        var date = new Date(date);
-        var newDate;
-        switch (type) {
-            case 'days': {
-                newDate = new Date(date.getFullYear(), date.getMonth(), date.getDate() + val, date.getHours(), date.getMinutes());
-                console.log('add days' + newDate);
-                break;
-            }
-            case 'months': {
-                newDate = new Date(date.getFullYear(), date.getMonth() + val, date.getDate(), date.getHours(), date.getMinutes());
-                console.log('add month');
-                break;
-            }
-            case 'hours': {
-                newDate = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours() + val, date.getMinutes());
-                break;
-            }
-            case 'minutes': {
-                newDate = new Date(date.getFullYear(), date.getMonth() + val, date.getDate(), date.getHours(), date.getMinutes() + val);
-                console.log('add min');
-                break;
-
-            }
+        case 'months': {
+            newDate = new Date(str.getFullYear(), str.getMonth() - units, str.getDate(), str.getHours(), str.getMinutes());
+            break;
         }
-        time.value = newDate;
+        case 'hours': {
+            newDate = new Date(str.getFullYear(), str.getMonth(), str.getDate(), str.getHours() - units, str.getMinutes());
+            break;
+        }
+        case 'minutes': {
+            newDate = new Date(str.getFullYear(), str.getMonth(), str.getDate(), str.getHours(), str.getMinutes() - units);
+            break;
+        }
+    }
+    time.value = newDate;
+    parseDate();
+}
+
+function checkData(units, type){
+    try{
+        if((parseInt(units)<0) || (type !== 'days' && type !== 'months' && type !== 'minutes' && type !== 'hours')){
+            throw new TypeError('check data');
+        }
+    }catch (e) {
+    }
+}
+
+function parseDate(){
+    var month =  time.value.getMonth();
+    if(month<9){
+        month = '0'+(month+1);
+    }
+    else {
+        month = month + 1;
+    }
+
+    var day =  time.value.getDate();
+    if(day<10){
+        day = '0'+day;
+    }
+    else {
+        day = day;
+    }
+
+    var hours =  time.value.getHours();
+    if(hours<10){
+        hours = '0'+hours;
+        console.log(hours);
+    }
+    else {
+        hours = hours;
+    }
+
+    var min =  time.value.getMinutes();
+    if(min<10){
+        min = '0'+min;
+    }
+    else {
+        min = min;
+    }
+
+    var str = time.value.getFullYear() + '-' + month + '-' + day + ' ' + hours + ':' + min;
+    time.value = str;
+}
+
+
+function date(str){
+    time.setValue(str)
+        .add(24, 'hours')
+        .subtract(1, 'months')
+        .add(3, 'days')
+        .add(15, 'minutes');
+
+}
+
+time = date('2017-05-16 13:45');
+var time = {
+    value: '',
+    getValue: function () {
+        return this.value;
+    },
+    setValue: function (value) {
+        this.value = value;
         return this;
-
-    }*/
-};
-
-Object.defineProperty(time, 'value', {
-    get: function () {
-        return this._value;
     },
-    set: function (value) {
-        this._value = value;
+    add: function (units, type) {
+        checkData(units, type);
+        addUnits(units, type);
+        return this;
     },
-});
-/*time = createObject('2017-05-12 13:45')
-    .add(24, 'hours')
-    .subtract(1, 'months')*/
-console.info(time.date('2017-05-12 13:45').add(24, 'hours').second());
+    subtract: function (units, type) {
+        checkData(units, type);
+        subtractUnits(units, type);
+        return this;
+    }
+}
+
+function addUnits(units, type) {
+    var str = new Date(time.value);
+    var newDate;
+    switch (type) {
+        case 'days': {
+            newDate = new Date(str.getFullYear(), str.getMonth(), str.getDate() + units, str.getHours(), str.getMinutes());
+            break;
+        }
+        case 'months': {
+            newDate = new Date(str.getFullYear(), str.getMonth() + units, str.getDate(), str.getHours(), str.getMinutes());
+            break;
+        }
+        case 'hours': {
+            newDate = new Date(str.getFullYear(), str.getMonth(), str.getDate(), str.getHours() + units, str.getMinutes());
+            break;
+        }
+        case 'minutes': {
+            newDate = new Date(str.getFullYear(), str.getMonth(), str.getDate(), str.getHours(), str.getMinutes() + units);
+            break;
+        }
+    }
+    time.value = newDate;
+    parseDate();
+}
+
+function subtractUnits(units, type){
+    var str = new Date(time.value);
+    var newDate;
+    switch (type) {
+        case 'days': {
+            newDate = new Date(str.getFullYear(), str.getMonth(), str.getDate() - units, str.getHours(), str.getMinutes());
+            break;
+        }
+        case 'months': {
+            newDate = new Date(str.getFullYear(), str.getMonth() - units, str.getDate(), str.getHours(), str.getMinutes());
+            break;
+        }
+        case 'hours': {
+            newDate = new Date(str.getFullYear(), str.getMonth(), str.getDate(), str.getHours() - units, str.getMinutes());
+            break;
+        }
+        case 'minutes': {
+            newDate = new Date(str.getFullYear(), str.getMonth(), str.getDate(), str.getHours(), str.getMinutes() - units);
+            break;
+        }
+    }
+    time.value = newDate;
+    parseDate();
+}
+
+function checkData(units, type){
+    try{
+        if((parseInt(units)<0) || (type !== 'days' && type !== 'months' && type !== 'minutes' && type !== 'hours')){
+            throw new TypeError('check data');
+        }
+    }catch (e) {
+    }
+}
+
+function parseDate(){
+    var month =  time.value.getMonth();
+    if(month<9){
+        month = '0'+(month+1);
+    }
+    else {
+        month = month + 1;
+    }
+
+    var day =  time.value.getDate();
+    if(day<10){
+        day = '0'+day;
+    }
+    else {
+        day = day;
+    }
+
+    var hours =  time.value.getHours();
+    if(hours<10){
+        hours = '0'+hours;
+        console.log(hours);
+    }
+    else {
+        hours = hours;
+    }
+
+    var min =  time.value.getMinutes();
+    if(min<10){
+        min = '0'+min;
+    }
+    else {
+        min = min;
+    }
+
+    var str = time.value.getFullYear() + '-' + month + '-' + day + ' ' + hours + ':' + min;
+    time.value = str;
+}
+
+
+function date(str){
+    time.setValue(str)
+        .add(24, 'hours')
+        .subtract(1, 'months')
+        .add(3, 'days')
+        .add(15, 'minutes');
+
+    console.log(time.value);
+
+}
+
+time = date('2017-05-16 13:45');
