79d12b8f706e0f67a2ac697f67952d80907c4313
Vladislav_K <v.kurashkevich@gmail.com>
2018-09-11 10:17:13 +0300

    some structure changes

---
+++ week4/jsTrainee/index.js
@@ -0,0 +1,20 @@
+var greeting = (function () {
+    var module = {};
+    var helloInLang = {
+        en: 'Hello world!',
+        es: '¡Hola mundo!',
+        ru: 'Привет, мир!'
+    };
+
+    module.getHello = function (lang) {
+        return helloInLang[lang];
+    };
+
+    module.writeHello = function (lang) {
+        document.write(module.getHello(lang))
+    };
+
+    return module;
+}());
+
+greeting.writeHello('en');
